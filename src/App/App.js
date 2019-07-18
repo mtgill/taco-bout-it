59fa5dac05d1e303d8911175157e99b61d6a19ff
Matt Gill <mtgill90@outlook.com>
2019-07-18 16:57:51 -0500

    setup complete

---
+++ src/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <button className="btn btn-warning">Warning</button>
+      </div>
+    );
+  }
+}
+
+export default App;
