01b7bf1ceeea594b960c33a207af1f762807a0d4
Matt Gill <mtgill90@outlook.com>
2019-07-18 17:42:34 -0500

    auth complete

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,42 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
 
+// Import firebase requirements
 import firebase from 'firebase/app';
 import 'firebase/auth';
-
-import Auth from '../components/Auth/Auth';
 import fbConnection from '../helpers/data/connection';
+
+// Import components
+import Auth from '../components/Auth/Auth';
+import Home from '../components/Home/Home';
 import MyNavBar from '../components/MyNavBar/MyNavBar';
 
+// Import styles
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 
 fbConnection();
+
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = props => (authed === false
+    ? (<Component {...props}/>)
+    : (<Redirect to={{ pathname: '/home', state: { from: props.location } }} />)
+  );
+  return <Route {...rest} render={props => routeChecker(props)} />;
+};
+
+const PrivateRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = props => (authed === true
+    ? (<Component {...props}/>)
+    : (<Redirect to={{ pathname: '/auth', state: { from: props.location } }} />)
+  );
+  return <Route {...rest} render={props => routeChecker(props)} />;
+};
 
 class App extends React.Component {
   state = {
@@ -36,7 +62,20 @@ class App extends React.Component {
 
     return (
       <div className="App">
+        <BrowserRouter>
+          <React.Fragment>
             <MyNavBar authed={authed} />
+            <div className="container">
+              <div className="row">
+                <Switch>
+                  <PublicRoute path="/auth" component={Auth} authed={authed} />
+                  <PrivateRoute path="/home" component={Home} authed={authed} />
+                  <Redirect from="*" to="/auth" />
+                </Switch>
+              </div>
+            </div>
+          </React.Fragment>
+        </BrowserRouter>
       </div>
     );
   }
