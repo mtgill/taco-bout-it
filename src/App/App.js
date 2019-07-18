ce26b34580a766271aacb1384edbdbf8ad24f178
Matt Gill <mtgill90@outlook.com>
2019-07-18 17:26:36 -0500

    wip - need to add routing to test auth

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,42 @@
 import React from 'react';
 
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import Auth from '../components/Auth/Auth';
+import fbConnection from '../helpers/data/connection';
 import MyNavBar from '../components/MyNavBar/MyNavBar';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 
+fbConnection();
+
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
+    const { authed } = this.state;
+
     return (
       <div className="App">
-        <MyNavBar />
+            <MyNavBar authed={authed} />
       </div>
     );
   }
