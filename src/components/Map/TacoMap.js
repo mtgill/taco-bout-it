0a830ea9691c0638ed6c0a40a655f68b3cbcc311
Matt Gill <mtgill90@outlook.com>
2019-07-18 19:24:31 -0500

    map displaying with a test layer and marker

--- src/components/Map/TacoMap.js
+++ src/components/Map/TacoMap.js
@@ -1,26 +1,38 @@
 import React from 'react';
 
-import { Map as LeafletMap, TileLayer } from 'react-leaflet';
+import {
+  Map,
+  TileLayer,
+  Marker,
+  Popup,
+} from 'react-leaflet';
 
 import './TacoMap.scss';
 
 
 class TacoMap extends React.Component {
   state = {
-    center: [36.1627, -86.7816],
+    lat: 36.1627,
+    lng: -86.7816,
     zoom: 12,
   }
 
   render() {
-    const { center, zoom } = this.state;
+    const { zoom } = this.state;
+    const center = [this.state.lat, this.state.lng];
     return (
       <div className="TacoMap">
-      <LeafletMap center={center} zoom={zoom}>
+      <Map center={center} zoom={zoom}>
          <TileLayer
           attribution='<a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-      </LeafletMap>
+        <Marker position={center}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </Map>
       </div>
     );
   }
