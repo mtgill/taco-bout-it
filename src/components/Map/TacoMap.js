77e8ab071eef631b365a8e1f2642048bf46df4d4
Matt Gill <mtgill90@outlook.com>
2019-07-18 19:15:08 -0500

    map is displaying!

--- src/components/Map/TacoMap.js
+++ src/components/Map/TacoMap.js
@@ -1,27 +1,26 @@
 import React from 'react';
 
-import L from 'leaflet';
-import { Map, TileLayer } from 'react-leaflet';
+import { Map as LeafletMap, TileLayer } from 'react-leaflet';
 
 import './TacoMap.scss';
 
 
 class TacoMap extends React.Component {
   state = {
-    center: [36.1627, 86.7816],
-    zoom: 13,
+    center: [36.1627, -86.7816],
+    zoom: 12,
   }
 
   render() {
     const { center, zoom } = this.state;
     return (
       <div className="TacoMap">
-      <Map>
-        <TileLayer
-          attribution='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+      <LeafletMap center={center} zoom={zoom}>
+         <TileLayer
+          attribution='<a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-      </Map>
+      </LeafletMap>
       </div>
     );
   }
