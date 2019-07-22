853c4e0d8f67c63f1393aff00e2d01a87ebf6ec7
Matt Gill <mtgill90@outlook.com>
2019-07-21 23:04:11 -0500

    wip

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
+import reviewData from '../../helpers/data/reviewData';
+
 import './Review.scss';
 
 class Review extends React.Component {
   render() {
+    const { review } = this.props;
+    console.error('review from reviews', review);
+    // const { tacoId } = this.props;
+    // const review = reviewData.getReviews(tacoId);
+    // this.getReviews(tacoId);
     return (
       <div className="Review">
         <h1>Reviews</h1>
+        <p>{review.comment}</p>
       </div>
     );
   }
--- src/components/SingleTaco/SingleTaco.js
+++ src/components/SingleTaco/SingleTaco.js
@@ -3,25 +3,33 @@ import React from 'react';
 import tacoData from '../../helpers/data/tacoData';
 import Review from '../Review/Review';
 
+import reviewData from '../../helpers/data/reviewData';
+
 import './SingleTaco.scss';
 
 class SingleTaco extends React.Component {
   state = {
     taco: {},
     location: '',
+    tacoId: '',
+    review: [],
   }
 
   componentDidMount() {
     const tacoId = this.props.match.params.id;
     const location = this.props.match.params.loc;
     this.setState({ location });
+    this.setState({ tacoId });
     tacoData.getSingleTaco(tacoId)
       .then(tacoPromise => this.setState({ taco: tacoPromise.data }))
       .catch(err => console.error('unable to get single taco', err));
+    reviewData.getReviews(tacoId)
+      .then(review => this.setState({ review: review[0] }));
   }
 
   render() {
-    const { taco, location } = this.state;
+    const { taco, location, review } = this.state;
+    console.error('review from single', review);
     return (
         <div className="SingleTaco col-10">
           <div className="card">
@@ -34,7 +42,7 @@ class SingleTaco extends React.Component {
             </div>
           </div>
           <div>
-            <Review />
+            <Review key={'review'} id={review.id} review={review} />
         </div>
         </div>
     );
