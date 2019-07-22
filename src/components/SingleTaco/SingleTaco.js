c7333ae1e54459b4b8eddf960702fd758b847cc7
Matt Gill <mtgill90@outlook.com>
2019-07-21 20:27:42 -0500

    singleTaco printing correctly

--- src/components/SingleTaco/SingleTaco.js
+++ src/components/SingleTaco/SingleTaco.js
@@ -7,27 +7,34 @@ import './SingleTaco.scss';
 class SingleTaco extends React.Component {
   state = {
     taco: {},
+    location: '',
   }
 
   componentDidMount() {
     const tacoId = this.props.match.params.id;
+    const location = this.props.match.params.loc;
+    this.setState({ location });
+    console.error(location);
     tacoData.getSingleTaco(tacoId)
       .then(tacoPromise => this.setState({ taco: tacoPromise.data }))
       .catch(err => console.error('unable to get single taco', err));
   }
 
   render() {
-    const { taco } = this.state;
+    const { taco, location } = this.state;
     return (
-      <div className="SingleTaco col-8">
-        <div className="card">
-          <div className="card-body">
-            <h3 className="card-title">{taco.name}</h3>
-            <h5 className="card-text">Ingredients: {taco.ingredients}</h5>
-            <button className="btn btn-success">Add Review</button>
+      // <div className="d-flex">
+        <div className="SingleTaco col-8">
+          <div className="card">
+            <div className="card-body">
+              <h3 className="card-title">{taco.name} - {location}</h3>
+              <h5 className="card-text">Location: {location}</h5>
+              <h5 className="card-text">Ingredients: {taco.ingredients}</h5>
+              <button className="btn btn-success">Add Review</button>
+            </div>
           </div>
         </div>
-      </div>
+      // </div>
     );
   }
 }
--- src/components/TacoPopup/TacoPopup.js
+++ src/components/TacoPopup/TacoPopup.js
@@ -7,22 +7,10 @@ import { Popup } from 'react-leaflet';
 import './TacoPopup.scss';
 
 class TacoPopup extends React.Component {
-  state = {
-    tacoId: '',
-  }
-
-  singleTaco = (e) => {
-    const { tacoId } = this.state;
-    e.preventDefault();
-    this.setState({ tacoId: e.target.key });
-    const { getSingleTaco } = this.props;
-    getSingleTaco(tacoId);
-  }
-
   render() {
     const { locationName, locationTacos } = this.props;
     const tacoNames = locationTacos.map(taco => (
-    <Link key={taco.name} id={taco.id} to={`/singleTaco/${taco.id}`}><li>{taco.name}</li></Link>));
+    <Link key={taco.name} loc={locationName} id={taco.id} to={`/singleTaco/${taco.id}/${locationName}`}><li>{taco.name}</li></Link>));
     return (
       <div className="TacoPopup">
         <Popup key={locationName}>
