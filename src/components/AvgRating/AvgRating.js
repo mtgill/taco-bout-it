958499ff58f7e9f45e17768072d0edb0f599b51a
Matt Gill <mtgill90@outlook.com>
2019-07-22 21:27:37 -0500

    ratings printing to console, need to convert new ratings to num

--- src/components/AvgRating/AvgRating.js
+++ src/components/AvgRating/AvgRating.js
@@ -1,12 +1,37 @@
 import React from 'react';
 
+import reviewData from '../../helpers/data/reviewData';
+
 import './AvgRating.scss';
 
 class AvgRating extends React.Component {
+  state = {
+    avg: 0,
+    allRatings: [],
+  }
+
+  getAverageRating = () => {
+    const { tacoId, avgRating } = this.props;
+    const { avg, allRatings } = this.state;
+    reviewData.getReviews(tacoId)
+      .then((reviews) => {
+        reviews.forEach((review) => {
+          allRatings.push(review.rating);
+        });
+        console.error('all ratings', allRatings);
+      });
+    this.setState({ allRatings });
+  }
+
+  componentDidMount() {
+    this.getAverageRating();
+  }
+
   render() {
+    const { allRatings } = this.state;
     return (
       <div className="AvgRating">
-        <h6>Avg Rating Here</h6>
+        <h6>{allRatings}</h6>
       </div>
     );
   }
