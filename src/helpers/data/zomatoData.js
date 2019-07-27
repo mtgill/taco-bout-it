0d252f3213b952215ca8dbaad3bc2f5167c77c6e
Matt Gill <mtgill90@outlook.com>
2019-07-27 08:54:33 -0500

    zomato data pulling correctly

--- src/helpers/data/zomatoData.js
+++ src/helpers/data/zomatoData.js
@@ -8,17 +8,4 @@ const config = { headers: { 'user-key': `${zomatoConfig.zomatoKeys.apiKey}` } };
 const getZomatoLocations = () => axios.get('https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&q=Taco', config);
 
 
-// const getZomatoLocations = () => new Promise((resolve, reject) => {
-//   axios.get('https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&q=Taco', config)
-//     .then((res) => {
-//       const zomLocs = [];
-//       res.data.forEach((restaurant) => {
-//         zomLocs.push(restaurant.name);
-//       });
-//       resolve(zomLocs);
-//     })
-//     .catch(err => reject(err));
-// });
-
-
 export default { getZomatoLocations };
